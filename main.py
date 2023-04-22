6d5f8a7fa6985b0c6f5ebf597a565246180c8a76
gato62 <infina62@mail.ru>
2023-04-22 20:50:17 +0300

    Add project 22.04.2023

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import constants as const
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((const.WIDTH, const.HEIGHT))
+
+
+if __name__ == '__main__':
+    main()
